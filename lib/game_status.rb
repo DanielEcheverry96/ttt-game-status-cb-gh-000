3a37331dd21e0bb6a9623456d3d4722fd64e22d2
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-01-24 06:38:17 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,9 +21,19 @@ def won?(board)
     if (position[0] == "X" && position[1] == "X" && position[2] == "X") || (position[0] == "O" && position[1] == "O" && position[2] == "O")
       WIN_COMBINATIONS[0]
     elsif (position[3] == "X" && position[4] == "X" && position[5] == "X") || (position[3] == "O" && position[4] == "O" && position[5] == "O")
-        WIN_COMBINATIONS[1]
+      WIN_COMBINATIONS[1]
     elsif (position[6] == "X" && position[7] == "X" && position[8] == "X") || (position[6] == "O" && position[7] == "O" && position[8] == "O")
-          WIN_COMBINATIONS[2]
+      WIN_COMBINATIONS[2]
+    elsif (position[0] == "X" && position[3] == "X" && position[6] == "X") || (position[0] == "O" && position[3] == "O" && position[6] == "O")
+      WIN_COMBINATIONS[3]
+    elsif (position[1] == "X" && position[4] == "X" && position[7] == "X") || (position[1] == "O" && position[4] == "O" && position[7] == "O")
+      WIN_COMBINATIONS[4]
+    elsif (position[2] == "X" && position[5] == "X" && position[8] == "X") || (position[2] == "O" && position[5] == "O" && position[8] == "O")
+      WIN_COMBINATIONS[5]
+    elsif (position[0] == "X" && position[4] == "X" && position[8] == "X") || (position[0] == "O" && position[4] == "O" && position[8] == "O")
+      WIN_COMBINATIONS[6]
+    elsif (position[2] == "X" && position[4] == "X" && position[6] == "X") || (position[2] == "O" && position[4] == "O" && position[6] == "O")
+      WIN_COMBINATIONS[7]
     end
   end
 end
